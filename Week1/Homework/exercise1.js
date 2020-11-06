da079e5100cb7c572e88274dd881e2e99c79a5c7
Marwa <makw90@hotmail.com>
2020-11-07 00:54:57 +0100

    added hello world in 10 languages

---
+++ Week1/Homework/exercise1.js
@@ -0,0 +1,22 @@
+let greeting ={
+    English : "Hello world!",// English
+    Arabic : "مرحبا بالعالم!",//Arabic
+    Dutch : "Hallo Wereld!",//Dutch
+    French:"Bonjour le monde!",//French
+    Russian :"Привет мир!",//Russian
+    Swedish :"Hej världen!",//Swedish
+    Indonesian:"Halo dunia!", // Indonesian
+    Italian :"Ciao mondo!", // Italian
+    Spanish :"Hola mundo!",// Spanish
+    Zulu :"Sawubona Mhlaba!"//Zulu
+}
+console.log(greeting.English)
+console.log(greeting.Arabic)
+console.log(greeting.Dutch)
+console.log(greeting.French)
+console.log(greeting.Russian)
+console.log(greeting.Swedish)
+console.log(greeting.Indonesian)
+console.log(greeting.Italian)
+console.log(greeting.Spanish)
+console.log(greeting.Zulu)
