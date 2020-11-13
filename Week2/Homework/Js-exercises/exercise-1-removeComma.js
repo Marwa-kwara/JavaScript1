b321951d468e87bf02739a116a5e3fff6825f333
Marwa <makw90@hotmail.com>
2020-11-13 14:06:20 +0100

    added javascript

---
+++ Week2/Homework/Js-exercises/exercise-1-removeComma.js
@@ -0,0 +1,7 @@
+let myString = 'hello,this,is,a,difficult,to,read,sentence';
+console.log(myString.length)
+console.log(myString)
+
+
+let replaceMyString= myString.replace(/,/g, " ");
+console.log(replaceMyString)
