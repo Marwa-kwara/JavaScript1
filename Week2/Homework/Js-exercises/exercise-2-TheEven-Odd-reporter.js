7d64b3191ed9005a04b89a4a7faf310dfff5c887
Marwa <makw90@hotmail.com>
2020-11-17 01:31:58 +0100

    added js

--- Week2/Homework/Js-exercises/exercise-2-TheEven-Odd-reporter.js
+++ Week2/Homework/Js-exercises/exercise-2-TheEven-Odd-reporter.js
@@ -1,4 +1,4 @@
 for (let i = 0; i<21; i++) {
     if (i%2!==0) console.log("The number" ,i ,"is odd!")
-    else if (i%2===0) console.log("The number" ,i ,"is even!")
+    else console.log("The number" ,i ,"is even!")
 }
